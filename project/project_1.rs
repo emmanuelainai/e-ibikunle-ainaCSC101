78c8cbf344b9fe9fe659378113ce0a265f380328
emmanuelainai <115726681+emmanuelainai@users.noreply.github.com>
2022-10-26 18:27:23 +0100

    Add files via upload

---
+++ project/project_1.rs
@@ -0,0 +1,12 @@
+project_1.rs
+fn main({
+	let p:f64 = 520000000.0;
+	let r:f64 = 10.0;
+	let t:f64 = 5.0;
+
+	let a = p * (1.0 +(r / 100.0)) * t;
+	println!("Amount is {}",a);
+	let ci = a - p;
+	println("Compund interest is {},ci");
+}
+)
